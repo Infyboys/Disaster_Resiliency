fc1c3a586e5a5dfc56a36ca4edbb36505fad84d5
rajanhhh <rajeshkumarnayak1994@gmail.com>
2020-07-18 21:23:38 +0530

    map changes

--- Disaster-Resiliency/src/main/java/com/ib/springbootstarter/controller/Controller.java
+++ Disaster-Resiliency/src/main/java/com/ib/springbootstarter/controller/Controller.java
@@ -1,6 +1,15 @@
 package com.ib.springbootstarter.controller;
 
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
@@ -9,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ib.springbootstarter.APIController.NewsAPIController;
+import com.ib.springbootstarter.beans.History;
 
 @org.springframework.stereotype.Controller
 @Component("MyController")
@@ -45,6 +55,34 @@ public class Controller {
 	public String news(Model model, String disaster, String place, String start, String end) {
 		 String news = NewsAPIController.getNews(disaster, place, start, end);
 		 model.addAttribute("message", news);
+		return "textResponse";
+	}
+	
+	@RequestMapping(value = "/history", method=RequestMethod.GET)
+	public String news(Model model,HttpServletRequest request, HttpServletResponse response,String disaster, String startDate, String endDate) {
+		try {
+			List<History> historyRecord =   services.getHistory(disaster,startDate,endDate);
+			
+			if(historyRecord.size() != 0) {
+				 ObjectMapper Obj = new ObjectMapper(); 
+				 String jsonStr = Obj.writeValueAsString(historyRecord);
+				 model.addAttribute("message", jsonStr);
+				 response.setStatus(200);
+			}else {
+				model.addAttribute("message", "No Data Found");
+				response.setStatus(400);
+			}
+		} catch (JsonGenerationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return "textResponse";
 	}
 }
--- Disaster-Resiliency/src/main/resources/static/js/mapLoader.js
+++ Disaster-Resiliency/src/main/resources/static/js/mapLoader.js
@@ -8,45 +8,6 @@ function moveMapToBerlin(map){
   map.setZoom(14);
 }
 
-function addMarkersToMap(map) {
-    var parisMarker = new H.map.Marker({lat:48.8567, lng:2.3508});
-    map.addObject(parisMarker);
-
-    var romeMarker = new H.map.Marker({lat:41.9, lng: 12.5});
-    map.addObject(romeMarker);
-
-    var berlinMarker = new H.map.Marker({lat:52.5166, lng:13.3833});
-    map.addObject(berlinMarker);
-
-    var madridMarker = new H.map.Marker({lat:40.4, lng: -3.6833});
-    map.addObject(madridMarker);
-
-    var londonMarker = new H.map.Marker({lat:51.5008, lng:-0.1224});
-    map.addObject(londonMarker);
-}
-
-function addSVGMarkers(map){
-  //Create the svg mark-up
-  var svgMarkup = '<svg  width="24" height="24" xmlns="http://www.w3.org/2000/svg">' +
-  '<rect stroke="black" fill="${FILL}" x="1" y="1" width="22" height="22" />' +
-  '<text x="12" y="18" font-size="12pt" font-family="Arial" font-weight="bold" ' +
-  'text-anchor="middle" fill="${STROKE}" >C</text></svg>';
-
-	/*var svgMarkup ='<svg height="210" width="500">' +
-	  '<polygon points="100,10 40,198 190,78 10,78 160,198" style="fill:lime;stroke:purple;stroke-width:5;fill-rule:nonzero;"/>' +
-	  'Sorry, your browser does not support inline SVG.'+
-	'</svg>';*/
-
-  // Add the first marker
-  var bearsIcon = new H.map.Icon(
-    svgMarkup),
-    bearsMarker = new H.map.Marker({lat:52.5166, lng:13.3833},
-      {icon: bearsIcon});
-
-  map.addObject(bearsMarker);
-
-}
-
 /**
  * Boilerplate map initialization code starts below:
  */
@@ -66,7 +27,7 @@ var map = new H.Map(document.getElementById('map'),
   pixelRatio: window.devicePixelRatio || 1
 });
 // add a resize listener to make sure that the map occupies the whole container
-window.addEventListener('resize', () = map.getViewPort().resize());
+window.addEventListener('resize', () => map.getViewPort().resize());
 
 //Step 3: make the map interactive
 // MapEvents enables the event system
@@ -78,7 +39,6 @@ var ui = H.ui.UI.createDefault(map, defaultLayers);
 
 // Now use the map as required...
 window.onload = function () {
-  moveMapToBerlin(map);
-  //addMarkersToMap(map);
-	addSVGMarkers(map);
+	//populateLocation();
+	
 }
